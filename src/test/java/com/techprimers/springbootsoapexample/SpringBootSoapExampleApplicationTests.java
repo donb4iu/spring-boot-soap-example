9ecbb501390926eb853b1fb219bd4742be81e53e
donb4iu <dbuddenbaum@gmail.com>
2024-07-10 20:27:23 -0400

    fixes for java version

--- src/test/java/com/techprimers/springbootsoapexample/SpringBootSoapExampleApplicationTests.java
+++ src/test/java/com/techprimers/springbootsoapexample/SpringBootSoapExampleApplicationTests.java
@@ -1,16 +1,18 @@
 package com.techprimers.springbootsoapexample;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
+
 @SpringBootTest
-public class SpringBootSoapExampleApplicationTests {
+class SpringBootSoapExampleApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
+
+
+
